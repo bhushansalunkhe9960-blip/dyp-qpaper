86ede57f89d66dbc979f6fac790bfbaeac1ecbeb
bhushansalunkhe9960-blip <bhushansalunkhe9960@gmail.com>
2026-05-09 14:43:39 +0530

    initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,56 @@
+plugins {
+    alias(libs.plugins.android.application)
+    id("com.google.gms.google-services")
+    id("com.google.firebase.crashlytics")
+}
+
+android {
+    namespace = "com.bhushansalunkhe.dypquestionpapers"
+    compileSdk {
+        version = release(36) {
+            minorApiLevel = 1
+        }
+    }
+
+    defaultConfig {
+        applicationId = "com.bhushansalunkhe.dypquestionpapers"
+        minSdk = 24
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+}
+
+dependencies {
+    implementation(libs.activity.ktx)
+    implementation(libs.appcompat)
+    implementation(libs.constraintlayout)
+    implementation(libs.material)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.espresso.core)
+    androidTestImplementation(libs.ext.junit)
+    // Import the BoM for the Firebase platform
+    implementation(platform("com.google.firebase:firebase-bom:34.13.0"))
+
+    // Add the dependency for the Firebase Authentication library
+    // When using the BoM, you don't specify versions in Firebase library dependencies
+    implementation("com.google.firebase:firebase-auth")
+    implementation("com.google.firebase:firebase-analytics")
+    implementation("com.google.firebase:firebase-crashlytics")
+}
---
+++ app/src/main/java/com/bhushansalunkhe/dypquestionpapers/MainActivity.java
@@ -0,0 +1,71 @@
+package com.bhushansalunkhe.dypquestionpapers;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btnLogin;
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+        // Check if user is signed in (non-null) and update UI accordingly
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser != null){
+            launchHomeScreen();
+        }
+
+        setContentView(R.layout.activity_main);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        btnLogin = findViewById(R.id.login_btn);
+
+        android.widget.EditText userNameInput = findViewById(R.id.username_input);
+        android.widget.EditText passWordNameInput = findViewById(R.id.password_input);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String userName = userNameInput.getText().toString().trim();
+                String password = passWordNameInput.getText().toString().trim();
+
+                if (userName.isBlank() || password.isBlank()) {
+                    Toast.makeText(MainActivity.this, "Username or password required", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                launchHomeScreen();
+
+            }
+        });
+    }
+
+    private void launchHomeScreen() {
+        // Launch the home screen
+        Intent homeScreenIntent = new Intent(MainActivity.this, HomeScreen.class);
+        startActivity(homeScreenIntent);
+        finish();
+    }
+}
