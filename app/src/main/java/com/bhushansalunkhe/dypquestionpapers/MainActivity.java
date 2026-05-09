7c26f19c0e9ab8f8f2443ccaec4e63075ca02834
bhushansalunkhe9960-blip <bhushansalunkhe9960@gmail.com>
2026-05-09 14:48:57 +0530

    added comment

--- app/src/main/java/com/bhushansalunkhe/dypquestionpapers/MainActivity.java
+++ app/src/main/java/com/bhushansalunkhe/dypquestionpapers/MainActivity.java
@@ -56,6 +56,7 @@ public class MainActivity extends AppCompatActivity {
                     return;
                 }
 
+                // Launch homescreen
                 launchHomeScreen();
 
             }
